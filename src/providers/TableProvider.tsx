d47a3dc53d914a268b6b26404939ec99e730b6ba
Kirill Nazarov <deeplogic2000@gmail.com>
2023-03-23 19:00:23 +0200

    fix: fixed define active cells logic

--- src/providers/TableProvider.tsx
+++ src/providers/TableProvider.tsx
@@ -13,17 +13,38 @@ type Props = {
 const TableProvider: FC<Props> = ({ data, rowsQuantity, children }) => {
   const [activeCells, setActiveCells] = useState<CellId[]>([]);
 
+  const filterData = (array: TypeCell[], index: number): number[] => {
+    console.log('array, index', array, index);
+    
+    if (index + 1 <= rowsQuantity / 2) {
+      const resultArr = array.slice(0, rowsQuantity + 1).map((cell: TypeCell) => cell.id)
+      console.log('resultArr index < rowsQuantity / 2', resultArr);
+      
+      return resultArr;
+    }
+    if (index >= array.length - rowsQuantity / 2) {
+      const resultArr = array.slice(- rowsQuantity - 1).map((cell: TypeCell) => cell.id)
+      console.log('index > array.length - rowsQuantity / 2', resultArr);
+      return resultArr;
+    }
+
+ return array
+      .filter(
+        (_, i) =>
+          i > index - rowsQuantity / 2 - 1 && i <= index + rowsQuantity / 2
+      )
+      .map((cell: TypeCell) => cell.id);
+  };
+
   const defineActiveCells = useCallback(
     (amount: CellValue, id: CellId) => {
-        const flatData = data.map((row: ITableRow) => row.values).flat();
-        flatData.sort((a: TypeCell, b: TypeCell) => a.amount - b.amount);
-        const index = flatData.findIndex((cell: TypeCell) => cell.id === id);
-        const results = flatData
-        .slice(index - rowsQuantity / 2, index + rowsQuantity / 2 + 1)
-        .map((cell: TypeCell) => cell.id);
-        setActiveCells(results);
+      const flatData = data.map((row: ITableRow) => row.values).flat();
+      flatData.sort((a: TypeCell, b: TypeCell) => a.amount - b.amount);
+      const index = flatData.findIndex((cell: TypeCell) => cell.id === id);
+      const results = filterData(flatData, index);
+      setActiveCells(results);
     },
-    [rowsQuantity, data]
+    [data, filterData]
   );
 
   const cancelHighlightCells = useCallback(() => {
